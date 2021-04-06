423e8996d1c0449b905df49c0a18aed26654f953
Aren <arenarzumanyan@gmail.com>
2021-04-06 15:57:45 +0400

    !!

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,87 +1,145 @@
 import request from '../helpers/request';
-import * as actionsTypes from './actionTypes';
-import {history} from '../helpers/history'
+import * as actionTypes from './actionTypes';
+import { history } from '../helpers/history';
+import { saveToken, requestWithoutToken } from '../helpers/auth';
 
-const apiHost = process.env.REACT_APP_API_URL
+const apiHost = process.env.REACT_APP_API_HOST
 
-export function getTasks(params= {}) {
-    const query = Object.entries(params).map(([key, value])=>`${key}=${value}`).join('&')
-    console.log(query)
+export function getTasks(params = {}) {
+    const query = Object.entries(params).map(([key, value]) => `${key}=${value}`).join('&')
     return (dispatch) => {
-        dispatch({ type: actionsTypes.PENDING })
+        dispatch({ type: actionTypes.PENDING })
         request(`${apiHost}/task?${query}`)
             .then((tasks) => {
-                dispatch({ type: actionsTypes.GET_TASKS, tasks: tasks })
+                if(!tasks) return;
+                dispatch({ type: actionTypes.GET_TASKS, tasks: tasks })
             })
             .catch((error) => {
-                console.log('error',  error)
-                dispatch({type: actionsTypes.ERROR, error: error.message})
+                console.log('error', error)
+                dispatch({ type: actionTypes.ERROR, error: error.message })
             });
     }
 }
 export function getTask(taskId) {
     return (dispatch) => {
-        dispatch({ type: actionsTypes.PENDING })
+        dispatch({ type: actionTypes.PENDING })
         request(`${apiHost}/task/${taskId}`)
             .then((task) => {
-                dispatch({ type: actionsTypes.GET_TASK, task })
+                if(!task) return;
+                dispatch({ type: actionTypes.GET_TASK, task })
             })
             .catch((error) => {
-                console.log('error',  error)
-                dispatch({type: actionsTypes.ERROR, error: error.message})
+                console.log('error', error)
+                dispatch({ type: actionTypes.ERROR, error: error.message })
             });
     }
 }
 
 export function addTask(newTask) {
     return (dispatch) => {
-        dispatch({ type: actionsTypes.PENDING })
+        dispatch({ type: actionTypes.PENDING })
         request(`${apiHost}/task`, 'POST', newTask)
             .then((task) => {
-                dispatch({ type: actionsTypes.ADD_TASK, task })
+                if(!task) return;
+                dispatch({ type: actionTypes.ADD_TASK, task })
             })
             .catch((error) => {
-                console.log('error',  error)
-                dispatch({type: actionsTypes.ERROR, error: error.message})
+                console.log('error', error)
+                dispatch({ type: actionTypes.ERROR, error: error.message })
             });
     }
 }
 export function deleteTask(taskId, from) {
     return (dispatch) => {
-        dispatch({ type: actionsTypes.PENDING })
+        dispatch({ type: actionTypes.PENDING })
         request(`${apiHost}/task/${taskId}`, 'DELETE')
-            .then(() => {
-                dispatch({ type: actionsTypes.DELETE_TASK, taskId, from })
-                if(from === 'single'){
+            .then((res) => {
+                if(!res) return;
+                dispatch({ type: actionTypes.DELETE_TASK, taskId, from })
+                if (from === 'single') {
                     history.push('/')
                 }
             })
             .catch((error) => {
-                dispatch({type: actionsTypes.ERROR, error: error.message})
+                dispatch({ type: actionTypes.ERROR, error: error.message })
             });
     }
 }
 export function deleteTasks(taskIds) {
     return (dispatch) => {
-        dispatch({ type: actionsTypes.PENDING })
-        request('http://localhost:3001/task/', 'PATCH', { tasks: [...taskIds] })
-            .then(() => {
-                dispatch({ type: actionsTypes.DELETE_TASKS, taskIds })
+        dispatch({ type: actionTypes.PENDING })
+        request(`${apiHost}/task/`, 'PATCH', { tasks: [...taskIds] })
+            .then((res) => {
+                if(!res) return;
+                dispatch({ type: actionTypes.DELETE_TASKS, taskIds })
             })
             .catch((error) => {
-                dispatch({type: actionsTypes.ERROR, error: error.message})
+                dispatch({ type: actionTypes.ERROR, error: error.message })
             });
     }
 }
 export function editTask(data, from) {
     return (dispatch) => {
-        dispatch({ type: actionsTypes.PENDING })
-        request(`http://localhost:3001/task/${data._id}`, 'PUT', data)
+        dispatch({ type: actionTypes.PENDING })
+        request(`${apiHost}/task/${data._id}`, 'PUT', data)
             .then((editedTask) => {
-                dispatch({ type: actionsTypes.EDIT_TASK, editedTask, from })
+                if(!editedTask) return;
+                dispatch({ type: actionTypes.EDIT_TASK, editedTask, from, status: data.status })
             })
             .catch((error) => {
-                dispatch({type: actionsTypes.ERROR, error: error.message})
+                dispatch({ type: actionTypes.ERROR, error: error.message })
+            });
+    }
+}
+export function register(data) {
+    return (dispatch) => {
+        dispatch({ type: actionTypes.PENDING })
+        requestWithoutToken(`${apiHost}/user`, 'POST', data)
+            .then((result) => {
+                dispatch({ type: actionTypes.REGISTER_SUCCESS })
+                history.push('/login')
+            })
+            .catch((error) => {
+                dispatch({ type: actionTypes.ERROR, error: error.message })
+            });
+    }
+}
+export function login(data) {
+    return (dispatch) => {
+        dispatch({ type: actionTypes.PENDING })
+        requestWithoutToken(`${apiHost}/user/sign-in`, 'POST', data)
+            .then((result) => {
+                saveToken(result);
+                dispatch({ type: actionTypes.LOGIN_SUCCESS });
+                history.push('/')
+            })
+            .catch((error) => {
+                dispatch({ type: actionTypes.ERROR, error: error.message })
+            });
+    }
+}
+export function logout(data) {
+    return (dispatch) => {
+        dispatch({ type: actionTypes.PENDING })
+        requestWithoutToken(`${apiHost}/user/sign-out`, 'POST', data)
+            .then(() => {
+                dispatch({ type: actionTypes.LOGOUT_SUCCESS });
+                history.push('/login')
+            })
+            .catch((error) => {
+                dispatch({ type: actionTypes.ERROR, error: error.message })
+            });
+    }
+}
+export function contact(data) {
+    return (dispatch) => {
+        dispatch({ type: actionTypes.PENDING })
+        requestWithoutToken(`${apiHost}/form`, 'POST', data)
+            .then(() => {
+                dispatch({ type: actionTypes.CONTACT_SUCCESS })
+            })
+            .catch((error) => {
+                dispatch({ type: actionTypes.ERROR, error: error.message })
             });
     }
 }
